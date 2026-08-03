7030b353bd5609906e5c0aeb091d18d8769930b9
rebecca3miller <rebecca.miller@keyin.com>
2026-08-03 21:27:37 -0230

    Fixing connection error.

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -6,7 +6,7 @@ public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://127.0.0.1:5432/postgres";
 
     private static final String USER = "postgres";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "document45";
 
     public static Connection getConnection()
     {
