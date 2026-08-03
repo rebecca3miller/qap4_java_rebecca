5a5b898b753edd56f8c258546ef6eba1fd2e5c5f
rebecca3miller <rebecca.miller@keyin.com>
2026-08-03 19:44:57 -0230

    set up patient java class, moving to file handler to save the drugs in file.

--- FileHandler.java
+++ FileHandler.java
@@ -1,3 +1,36 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FileHandler {
-    
+    public static void saveDrugToFile(Drug drug) {
+        try {
+            FileWriter fileWriter = new FileWriter("drugs.txt", true);
+            fileWriter.write(drug.toString() + "\n");
+            fileWriter.close();
+            System.out.println("Drug saved to file successfully.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while saving the drug to file.");
+            e.printStackTrace();
+        }
+    }
+}
+
+public static void readDrugsFromFile() {
+    try {
+        File file = new File("drugs.txt");
+        Scanner fileReader = new Scanner(file);
+        
+        System.out.println("Drugs in file:");
+
+        while (fileReader.hasNextLine()) {
+            String drugData = fileReader.nextLine();
+            System.out.println(drugData);
+        }
+        fileReader.close();
+    } catch (IOException e) {
+        System.out.println("An error occurred while reading the drugs from file.");
+        e.printStackTrace();
+    }
 }
---
+++ Patient.java
@@ -0,0 +1,56 @@
+public class Patient {
+    private int patientId;
+    private String patientFirstName;
+    private String patientLastName;
+    private String patientDOB;
+
+    public Patient(int patientId, String patientFirstName, String patientLastName, String patientDOB) {
+        this.patientId = patientId;
+        this.patientFirstName = patientFirstName;
+        this.patientLastName = patientLastName;
+        this.patientDOB = patientDOB;
+    }
+
+    // Getters and setters
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getPatientFirstName() {
+        return patientFirstName;
+    }
+
+    public void setPatientFirstName(String patientFirstName) {
+        this.patientFirstName = patientFirstName;
+    }
+
+    public String getPatientLastName() {
+        return patientLastName;
+    }
+
+    public void setPatientLastName(String patientLastName) {
+        this.patientLastName = patientLastName;
+    }
+
+    public String getPatientDOB() {
+        return patientDOB;
+    }
+
+    public void setPatientDOB(String patientDOB) {
+        this.patientDOB = patientDOB;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "patientId=" + patientId +
+                ", patientFirstName='" + patientFirstName + '\'' +
+                ", patientLastName='" + patientLastName + '\'' +
+                ", patientDOB='" + patientDOB + '\'' +
+                '}';
+    }
+}
