81580060dad79bab760b6ea57645009ed966a8a8
rebecca3miller <rebecca.miller@keyin.com>
2026-08-03 19:57:00 -0230

    file successfully saved drugs to file.

--- FileHandler.java
+++ FileHandler.java
@@ -15,22 +15,23 @@ public class FileHandler {
             e.printStackTrace();
         }
     }
-}
 
-public static void readDrugsFromFile() {
-    try {
-        File file = new File("drugs.txt");
-        Scanner fileReader = new Scanner(file);
+
+    public static void readDrugsFromFile() {
+        try {
+            File file = new File("drugs.txt");
+            Scanner fileReader = new Scanner(file);
         
-        System.out.println("Drugs in file:");
+            System.out.println("Drugs in file:");
 
-        while (fileReader.hasNextLine()) {
-            String drugData = fileReader.nextLine();
-            System.out.println(drugData);
+            while (fileReader.hasNextLine()) {
+                String drugData = fileReader.nextLine();
+                System.out.println(drugData);
+            }
+            fileReader.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the drugs from file.");
+            e.printStackTrace();
         }
-        fileReader.close();
-    } catch (IOException e) {
-        System.out.println("An error occurred while reading the drugs from file.");
-        e.printStackTrace();
     }
 }
