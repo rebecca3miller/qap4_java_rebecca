1a16003e8471703e9a7b8a2175057c7751347731
rebecca3miller <rebecca.miller@keyin.com>
2026-08-03 19:48:24 -0230

    Testing main to see if drugs were saved to the file.

--- Main.java
+++ Main.java
@@ -1,3 +1,13 @@
 public class Main {
+
+    public static void main(String[] args) {
+        Drug drug1 = new Drug (1, "Tylenol", 5.99, "500mg");
+        Drug drug2 = new Drug (2, "Advil", 6.99, "200mg");
+
+        FileHandler.saveDrugToFile(drug1);
+        FileHandler.saveDrugToFile(drug2);
+
+        FileHandler.readDrugsFromFile();
+    }
     
 }
